04794614a0de8517c686266c1ddea0417def4a0a
AWAISOEM <awaisoem@gmail.com>
2024-09-03 00:00:56 +0500

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script  from "next/script";
-
+import { Analytics } from "@vercel/analytics/react"
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
+        <Analytics/>
         <NextScript />
       </body>
     </Html>
