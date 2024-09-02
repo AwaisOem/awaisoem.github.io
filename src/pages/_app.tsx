4b0de0c6eacdd86e7daba211b57317e30a7d143d
AWAISOEM <awaisoem@gmail.com>
2024-09-02 23:47:43 +0500

    font-change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,30 @@
 import "@/scss/globals.css";
 import "@/scss/index.scss";
 import type { AppProps } from "next/app";
-import { Raleway, Fira_Code } from "next/font/google";
+// import { Fira_Code, Raleway } from "next/font/google";
 
-const raleway = Raleway({ subsets: ["latin"] });
-const firaCode = Fira_Code({
-  weight: ["300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-});
+// const raleway = Raleway({ subsets: ["latin"] });
+// const firaCode = Fira_Code({
+//   weight: ["300", "400", "500", "600", "700"],
+//   subsets: ["latin"],
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <style jsx global>{`
+      @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');
         :root {
-          --raleway: ${raleway.style.fontFamily};
-          --fira-code: ${firaCode.style.fontFamily};
+          --raleway: 'Raleway';
+          --fira-code: 'Fira Code';
         }
       `}</style>
       <Component {...pageProps} />;
     </>
   );
 }
+// :root {
+//   --raleway: ${raleway.style.fontFamily};
+//   --fira-code: ${firaCode.style.fontFamily};
+// }
